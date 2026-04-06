b916a8d0745a8c814e503d12dedbec044e5aca6f
Kiko <kikoguirado@gmail.com>
2026-04-06 14:24:32 +0200

    feat: implement conditional search display in navbar based on current route

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs/operators';
 import { LucideSearch, LucideDynamicIcon } from '@lucide/angular';
 import { SearchService } from '../../core/services/search.service';
 
@@ -12,6 +15,22 @@ import { SearchService } from '../../core/services/search.service';
 export class Navbar {
     protected readonly LucideSearch = LucideSearch;
 
+    private readonly router = inject(Router);
     private readonly searchService = inject(SearchService);
     protected readonly searchQuery = this.searchService.query;
+
+    private readonly currentUrl = signal(this.router.url);
+    protected readonly showSearch = computed(() => this.currentUrl() === '/collections');
+
+    constructor() {
+        this.router.events
+            .pipe(filter((e) => e instanceof NavigationEnd), takeUntilDestroyed())
+            .subscribe((e) => {
+                const url = (e as NavigationEnd).urlAfterRedirects;
+                this.currentUrl.set(url);
+                if (url !== '/collections') {
+                    this.searchService.query.set('');
+                }
+            });
+    }
 }
